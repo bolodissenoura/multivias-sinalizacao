e1cf17a4aca02c088d5ee3ce5d6c74945ba1733e
apolorezende <apolo_henriquerr@hotmail.com>
2023-08-28 18:53:56 -0300

    data text edit

--- components/data.js
+++ components/data.js
@@ -11,46 +11,46 @@ import benefitOneImg from "../public/img/benefit-one.png";
 import benefitTwoImg from "../public/img/benefit-two.png";
 
 const benefitOne = {
-  title: "Highlight your benefits",
-  desc: "You can use this space to highlight your first benefit or a feature of your product. It can also contain an image or Illustration like in the example along with some bullet points.",
+  title: "Lorem, ipsum dolor sit",
+  desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus eum nihil ea quaerat possimus enim harum, excepturi velit quisquam sequi repudiandae consequatur provident similique, esse dicta illo, in asperiores atque fugiat quia eius. Voluptatibus libero quibusdam dolorem .",
   image: benefitOneImg,
   bullets: [
     {
-      title: "Understand your customers",
-      desc: "Then explain the first point breifly in one or two lines.",
+      title: "Lorem, ipsum dolor sit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus eum nihil ea quaerat possimus enim harum, excepturi velit quisquam sequi repudiandae consequatur provident similique, esse dicta illo, in asperiores atque fugiat quia eius. Voluptatibus libero quibusdam dolorem ",
       icon: <FaceSmileIcon />,
     },
     {
-      title: "Improve acquisition",
-      desc: "Here you can add the next benefit point.",
+      title: "Lorem, ipsum dolor sit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
       icon: <ChartBarSquareIcon />,
     },
     {
-      title: "Drive customer retention",
-      desc: "This will be your last bullet point in this section.",
+      title: "Lorem, ipsum dolor sit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
       icon: <CursorArrowRaysIcon />,
     },
   ],
 };
 
 const benefitTwo = {
-  title: "Offer more benefits here",
-  desc: "You can use this same layout with a flip image to highlight your rest of the benefits of your product. It can also contain an image or Illustration as above section along with some bullet points.",
+  title: "Lorem, ipsum dolor sit",
+  desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit",
   image: benefitTwoImg,
   bullets: [
     {
-      title: "Mobile Responsive Template",
-      desc: "Nextly is designed as a mobile first responsive template.",
+      title: "Lorem, ipsum dolor sit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit",
       icon: <DevicePhoneMobileIcon />,
     },
     {
-      title: "Powered by Next.js & TailwindCSS",
-      desc: "This template is powered by latest technologies and tools.",
+      title: "Lorem, ipsum dolor sit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit",
       icon: <AdjustmentsHorizontalIcon />,
     },
     {
-      title: "Dark & Light Mode",
-      desc: "Nextly comes with a zero-config light & dark mode. ",
+      title: "Lorem, ipsum dolor sit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit",
       icon: <SunIcon />,
     },
   ],
