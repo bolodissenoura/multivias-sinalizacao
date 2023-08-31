dc226fda43868bdfa57acbb6c0b9bab1f0bd2ea5
apolorezende <apolo_henriquerr@hotmail.com>
2023-08-31 16:50:09 -0300

    feat: alien items hero page

--- components/hero.js
+++ components/hero.js
@@ -4,16 +4,15 @@ import Container from "./container";
 const Hero = () => {
   return (
     <>
-      <Container className="flex flex-wrap ">
-        <div className="flex items-center w-full lg:w-1/2">
-          <div className="max-w-2xl mb-8">
+      <Container className="flex flex-wrap">
+        <div className="flex flex-col justify-center items-center w-full">
+          <div className="flex flex-col justify-center items-center max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
               Multivias Sinalização.
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
               Somos o caminho certo para deixar sua rodovia mais segura.
             </p>
-
             <div className="flex flex-col items-center space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
               <a
                 href="https://google.com/"
