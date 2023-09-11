662e3aa6dbfc9342e6af42031bc0976952b68f9a
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-09-11 16:18:48 -0300

    fix: resend function

--- pages/api/send.js
+++ pages/api/send.js
@@ -1,25 +1,9 @@
 import { Resend } from "resend";
 import { NextResponse } from "next/server";
 
-const resend = new Resend('re_gtL4pWF3_LKTFJtxBe8xy6SEssFLetiaz');
+
 
 export const sendEmail = async () => {
-  await resend.emails.send({
-    from: "Acme <onboarding@resend.dev>",
-    to: ["delivered@resend.dev"],
-    subject: "hello world",
-    text: "it works!",
-    headers: {
-      "X-Entity-Ref-ID": "123456789",
-    },
-
-    tags: [
-      {
-        name: "category",
-        value: "confirm_email",
-      },
-    ],
-  });
   // try {
   //   const data = await resend.emails.send({
   //     from: [email],
@@ -27,7 +11,6 @@ export const sendEmail = async () => {
   //     subject: [subject],
   //     react: [message],
   //   });
-
   //   res?.status(200).json(data);
   // } catch (error) {
   //   res?.status(400).json(error);
